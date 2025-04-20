b5baccaf74b0964e41a4c08408ab71c9991bde8a
yosrkharrat <yosr.kharrat@insat.ucar.tn>
2025-04-20 23:16:49 +0100

    fixed link

--- index.py
+++ index.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, send_file, render_template
-
+import yt_dlp
+import os
+app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html")  # au lieu de render_template_string
+    return render_template("index.html"
 
-import yt_dlp
 
-url = input("paste YouTube URL here")
-ydl_opts = {
-    'format': 'bestvideo+bestaudio/best',  # Best quality video+audio
-    'outtmpl': '%(title)s.%(ext)s'         # Output file name as the video title
-}
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s'
+    }
 
-with yt_dlp.YoutubeDL() as ydl:
-    info = ydl.extract_info(url, download=False)
-    print("Title:", info['title'])
-    ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+    except Exception as e:
+        return f"Error during download: {e}"
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
